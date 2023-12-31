456d69862e141dcbee7e19e61acba6d78c6a9bf0
Dean Le <xuannhak13@gmail.com>
2023-12-31 22:00:42 +0700

    integrate chart api (#11)

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { createChart, ColorType } from "lightweight-charts";
+
+export type LineChartData = {
+  time: string;
+  value: string | number;
+};
+
+type LineChartOption = {
+  id: string;
+  data: LineChartData[];
+  lineColor?: string;
+};
+
+type LineChartProps = {
+  options: LineChartOption[];
+};
+
+const LineChart = (props: LineChartProps) => {
+  const { options } = props;
+
+  const chartContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current?.clientWidth });
+    };
+
+    const chart = createChart(chartContainerRef.current || "", {
+      layout: {
+        background: { type: ColorType.Solid, color: "transparent" },
+        textColor: "#848E9C",
+        fontSize: 12,
+      },
+      width: chartContainerRef.current?.clientWidth,
+      height: chartContainerRef.current?.clientHeight,
+      grid: {
+        vertLines: {
+          visible: false,
+        },
+        horzLines: {
+          visible: false,
+        },
+      },
+      localization: {
+        priceFormatter: (v: string | number) =>
+          `${Math.round(Number(v) * 100) / 100}%`,
+      },
+    });
+    chart.timeScale().fitContent();
+
+    options.map((option) => {
+      chart
+        .addLineSeries({
+          lineWidth: 1,
+          color: option.lineColor,
+          priceLineVisible: false,
+          // lastValueVisible: false,
+        })
+        .setData(option.data);
+    });
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+
+      chart.remove();
+    };
+  }, [options]);
+
+  return <div ref={chartContainerRef} className="w-full h-full" />;
+};
+
+export default LineChart;
---
+++ src/components/VaultChart.tsx
@@ -0,0 +1,66 @@
+import LineChart, { LineChartData } from "./LineChart";
+
+type VaultChartProps = {
+  apr: number;
+  marketData: LineChartData[];
+  onyxData: LineChartData[];
+};
+
+const VaultChart = (props: VaultChartProps) => {
+  const { apr, marketData, onyxData } = props;
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <button type="button" className="rounded-full px-3 text-rock-gray">
+            Week
+          </button>
+          <button
+            type="button"
+            className="bg-rock-button rounded-full px-6 py-2"
+          >
+            Month
+          </button>
+          <button type="button" className="rounded-full px-3 text-rock-gray">
+            Year
+          </button>
+        </div>
+        <div className="flex items-center gap-10">
+          <div className="flex items-center gap-2">
+            <span className="w-5 h-[1px] bg-[#9EB50E]" />
+            <p className="text-rock-gray">Market</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="w-5 h-[1px] bg-[#0057FF]" />
+            <p className="text-rock-gray">Onyx Vault *</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center gap-3 mt-6 pl-4">
+        <p className="text-xl">APR</p>
+        <span className="px-2 py-0.5 border border-rock-green border-opacity-40 rounded-md text-rock-green font-semibold">
+          {`${Math.round(apr)}%`}
+        </span>
+      </div>
+      <div className="h-[300px]">
+        <LineChart
+          options={[
+            {
+              id: "market",
+              data: marketData,
+              lineColor: "#9EB50E",
+            },
+            {
+              id: "onyx",
+              data: onyxData,
+              lineColor: "#0057FF",
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
+};
+
+export default VaultChart;
