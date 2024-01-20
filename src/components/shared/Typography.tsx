0ebd7acb9fde4b59e283d5da8833aa5b31b2e840
Nha Le <xuannhak13@gmail.com>
2024-01-20 15:50:34 +0700

    add withdraw tooltip

---
+++ src/components/shared/Typography.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+
+type TypographyProps = {
+  variant?: 'body' | 'heading' | 'subtitle';
+  className?: string;
+  children: ReactNode;
+};
+
+const Typography = (props: TypographyProps) => {
+  const { variant = 'body', className = '', children } = props;
+
+  const typoClass = {
+    heading: 'text-xl md:text-2xl lg:text-4xl font-normal uppercase font-bruno-ace',
+    subtitle: 'font-bruno-ace uppercase text-gray-200',
+    body: 'text-base font-light text-gray-400',
+  }[variant];
+
+  return <p className={`${typoClass} ${className}`}>{children}</p>;
+};
+
+export default Typography;
