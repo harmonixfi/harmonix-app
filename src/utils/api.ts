2768601d8a4fa9d632cea5b891125c50fc9a58ea
Bean Nguyen <bao.nlq94@gmail.com>
2024-02-09 10:17:12 +0700

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const apiFetch = async <T>(url: string) => {
   // revalidate at most every hour
-  const res = await fetch(`${baseUrl}${url}`, { next: { revalidate: 3600 } });
+  const res = await fetch(`${baseUrl}${url}`, { next: { revalidate: 5*60 } });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
