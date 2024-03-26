4c778a1c50821fd526f2140b0a5e68a4b5657875
Dean Le <xuannhak13@gmail.com>
2024-03-26 14:37:36 +0700

    set no cache (#43)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,7 @@
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const apiFetch = async <T>(url: string) => {
-  // revalidate at most every hour
-  const res = await fetch(`${baseUrl}${url}`, { next: { revalidate: 5 * 60 } });
+  const res = await fetch(`${baseUrl}${url}`, { cache: 'no-store' });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
