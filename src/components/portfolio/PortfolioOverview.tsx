f9807c493cea05de095bb0be5231fc587a876df1
Dean Le <xuannhak13@gmail.com>
2024-03-30 00:11:09 +0700

    show portfolio loading (#54)

--- src/components/portfolio/PortfolioOverview.tsx
+++ src/components/portfolio/PortfolioOverview.tsx
@@ -7,46 +7,64 @@ import { formatPnl, toFixedNumber } from '@/utils/number';
 import Typography from '../shared/Typography';
 
 type PortfolioOverviewProps = {
+  loading: boolean;
+  error: boolean;
   totalBalance?: number;
   pnl?: number;
 };
 
 const PortfolioOverview = (props: PortfolioOverviewProps) => {
-  const { totalBalance = 0, pnl = 0 } = props;
+  const { loading, error, totalBalance = 0, pnl = 0 } = props;
 
   const { status } = useAccount();
 
-  const isConnectedWallet = status === 'connected';
+  if (status === 'connecting' || loading) {
+    return (
+      <div>
+        <Typography variant="subheading">Portfolio Overview</Typography>
+        <div className="sm:w-[360px] xl:w-[400px] h-40 bg-white bg-opacity-10 border border-rock-divider rounded-2xl mt-4 lg:mt-8 animate-pulse" />
+      </div>
+    );
+  }
+
+  if (status !== 'connected') {
+    return (
+      <p className="text-rock-sub-body text-base md:text-xl text-center">
+        Connect your wallet to view your portfolio
+      </p>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <Typography variant="subheading">Portfolio Overview</Typography>
+        <p className="text-red-600 mt-4">Oops, something went wrong! Please try again later.</p>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {!isConnectedWallet ? (
-        <p className="text-rock-sub-body text-base md:text-xl text-center">
-          Connect your wallet to view your portfolio
-        </p>
-      ) : (
-        <>
-          <Typography variant="subheading">Portfolio Overview</Typography>
+      <Typography variant="subheading">Portfolio Overview</Typography>
 
-          <div className="sm:w-[360px] xl:w-[400px] bg-white bg-opacity-10 border border-rock-divider p-6 md:p-8 xl:p-11 rounded-2xl mt-4 lg:mt-8">
-            <p className="text-sm md:text-base xl:text-xl uppercase text-rock-gray leading-3">
-              Your balance
-            </p>
-            <div className="flex items-center gap-6 mt-6 lg:mt-10">
-              <p className="text-sm md:text-xl xl:text-2xl font-semibold leading-4">
-                {toFixedNumber(totalBalance)} USDC
-              </p>
-              <p
-                className={`text-sm md:text-xl xl:text-2xl leading-4 font-normal ${
-                  toFixedNumber(pnl) >= 0 ? 'text-rock-green' : 'text-red-600'
-                }`}
-              >
-                {formatPnl(toFixedNumber(pnl), true)}
-              </p>
-            </div>
-          </div>
-        </>
-      )}
+      <div className="sm:w-[360px] xl:w-[400px] bg-white bg-opacity-10 border border-rock-divider p-6 md:p-8 xl:p-11 rounded-2xl mt-4 lg:mt-8">
+        <p className="text-sm md:text-base xl:text-xl uppercase text-rock-gray leading-3">
+          Your balance
+        </p>
+        <div className="flex items-center gap-6 mt-6 lg:mt-10">
+          <p className="text-sm md:text-xl xl:text-2xl font-semibold leading-4">
+            {toFixedNumber(totalBalance)} USDC
+          </p>
+          <p
+            className={`text-sm md:text-xl xl:text-2xl leading-4 font-normal ${
+              toFixedNumber(pnl) >= 0 ? 'text-rock-green' : 'text-red-600'
+            }`}
+          >
+            {formatPnl(toFixedNumber(pnl), true)}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
