e3cfe10fca8dad0efe5cc4f6956dd4646d3df848
Nha Le <xuannhak13@gmail.com>
2024-06-01 21:57:04 +0700

    update feedback link

--- src/components/shared/FeedbackButton.tsx
+++ src/components/shared/FeedbackButton.tsx
@@ -5,7 +5,7 @@ import { ExternalLinkIcon } from './icons';
 const FeedbackButton = () => {
   return (
     <Link
-      href="https://rockonyx.featurebase.app/"
+      href="https://harmonix.featurebase.app"
       target="_blank"
       className="fixed top-1/2 -right-10 sm:-right-12 -translate-y-1/2 flex items-center gap-3 bg-rock-highlight -rotate-90 text-xs sm:text-sm font-light px-3 sm:px-6 py-1.5 sm:py-2 rounded-xl z-40 hover:bg-rock-light-blue hover:ring-1 hover:ring-rock-highlight transition-all duration-200"
     >
