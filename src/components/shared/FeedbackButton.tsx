8b10029082ae9cf74e2c6db4f3ba328ef4cb1891
Dean Le <xuannhak13@gmail.com>
2024-06-09 23:16:06 +0700

    display audit and feedback button (#131)

--- src/components/shared/FeedbackButton.tsx
+++ src/components/shared/FeedbackButton.tsx
@@ -1,17 +1,21 @@
+import { Button } from '@nextui-org/react';
 import Link from 'next/link';
 
 import { ExternalLinkIcon } from './icons';
 
 const FeedbackButton = () => {
   return (
-    <Link
+    <Button
+      as={Link}
       href="https://harmonix.featurebase.app"
       target="_blank"
-      className="fixed top-1/2 -right-10 sm:-right-12 -translate-y-1/2 flex items-center gap-3 bg-rock-highlight -rotate-90 text-xs sm:text-sm font-light px-3 sm:px-6 py-1.5 sm:py-2 rounded-xl z-40 hover:bg-rock-light-blue hover:ring-1 hover:ring-rock-highlight transition-all duration-200"
+      size="sm"
+      color="primary"
+      className="fixed top-1/2 -right-12 -translate-y-1/2 flex items-center gap-3 px-6 -rotate-90 z-40"
     >
       Feedback
-      <ExternalLinkIcon className="w-3 h-3 sm:w-4 sm:h-4" />
-    </Link>
+      <ExternalLinkIcon className="w-3 h-3" />
+    </Button>
   );
 };
 
