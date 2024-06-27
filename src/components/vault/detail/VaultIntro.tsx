6fdb036dc0344b7bd382e93389ad548b21b8ddce
Nha Le <xuannhak13@gmail.com>
2024-06-27 23:18:04 +0700

    update bsx vault

--- src/components/vault/detail/VaultIntro.tsx
+++ src/components/vault/detail/VaultIntro.tsx
@@ -6,6 +6,7 @@ import { Card } from '@nextui-org/react';
 import Link from 'next/link';
 
 import { Point } from '@/@types/portfolio';
+import { BsxIcon } from '@/components/shared/icons';
 import { toFixedNumber, withCommas } from '@/utils/number';
 import { getDisplayedPoint } from '@/utils/vault';
 
@@ -34,26 +35,37 @@ const VaultIntro = (props: VaultIntroProps) => {
       return 'Increase yield by converting half of the fund deposit into ETH and re-staking it on KelpDAO. Meanwhile, exchange the other half for stablecoins and open a 1x short position on decentralized derivative exchanges.';
     }
 
+    if (slug.includes('base')) {
+      return 'Generating yield by shorting ETH on BSX with a favorable funding rate, while holding ETH in spot or yield to be neutral delta against USD.';
+    }
+
     return 'Generating yield by shorting ETH on a perp markets with a favorable funding rate, while holding ETH in spot or yield to be neutral delta against USD.';
   }, [slug]);
 
   return (
     <Card className="flex flex-col 2xl:flex-row items-center gap-12 rounded-2xl p-8">
-      <div className="space-y-3">
-        <p className="text-3xl sm:text-4xl font-bold">{name}</p>
-        <p className="text-base sm:text-lg font-light">
-          {description}{' '}
-          <Link
-            href="https://harmonix-finance.gitbook.io/harmonix-docs/harmonix-vaults/overview-vaults"
-            target="_blank"
-            className="underline font-semibold"
-          >
-            Learn more
-          </Link>
-        </p>
+      <div className="flex items-center gap-3">
+        {slug.includes('base') && (
+          <span className="shrink-0 flex items-center justify-center w-32 h-32 bg-indigo-200 rounded-full partner-intro">
+            <BsxIcon className="w-20 h-20" />
+          </span>
+        )}
+        <div className="space-y-2 lg:space-y-3">
+          <p className="text-3xl sm:text-4xl font-bold">{name}</p>
+          <p className="text-base sm:text-lg font-light">
+            {description}{' '}
+            <Link
+              href="https://harmonix-finance.gitbook.io/harmonix-docs/harmonix-vaults/overview-vaults"
+              target="_blank"
+              className="underline font-semibold"
+            >
+              Learn more
+            </Link>
+          </p>
+        </div>
       </div>
       {points && points.length > 0 && (
-        <div className="shrink-0 w-full 2xl:w-auto flex flex-col md:flex-row flex-wrap items-center justify-between gap-12 bg-secondary text-primary rounded-2xl px-6 py-8">
+        <div className="shrink-0 w-full 2xl:w-auto flex flex-col md:flex-row flex-wrap items-center justify-around gap-12 bg-secondary text-primary rounded-2xl px-6 py-8">
           {points.map((x) => {
             const point = getDisplayedPoint(x);
             if (!point) {
